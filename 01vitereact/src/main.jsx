f638d65a6b9182e3b7c3d321b8e2f5f2d484895a
Gautam Adhikari <gautamadhikari831@gmail.com>
2024-07-16 16:15:53 +0530

    day 03

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App !</h1>
+        </div>
+    )
+}
+
+// const ReactElement= {
+//     type: 'a',
+//     props: {
+//         href: 'https://www.google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click here! To visit google'
+// }
+
+const reactElement = React.createElement(
+    'a',
+    {
+        href : 'https://www.google.com',
+        target : '_blank'
+    },
+    'click to visit google'
+)
+
+const anotherElement = (
+    <a href="https://google.com" tager='_blank'>visit google</a>
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    // <App />
+    // <MyApp/>
+    // MyApp()
+    // ReactElement
+    // anotherElement
+    reactElement
 )
